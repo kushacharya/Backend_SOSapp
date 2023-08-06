4f905aa4c3be5859e739f296e390ccafeab42a06
kushacharya <ktacharya15318@gmail.com>
2023-08-06 19:59:46 +0530

    updated cors in app.use

--- NARI_SSIP/app.js
+++ NARI_SSIP/app.js
@@ -3,11 +3,13 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routers/user.js';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3000;
 const MONGODB = process.env.MONGODB;
 
 const app = express();
+app.use(cors());
 app.use(express.json({limit: '75mb'}));
 app.use('/api/user', router);
 app.use(express.static('public'));
